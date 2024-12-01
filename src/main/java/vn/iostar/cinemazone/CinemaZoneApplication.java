16fea5253d5aaf33d91d98049f3f8e66ecc2362d
81quanghuy <20110650@student.hcmute.edu.vn>
2024-12-02 00:31:18 +0700

    update code 2/12

---
+++ src/main/java/vn/iostar/cinemazone/CinemaZoneApplication.java
@@ -0,0 +1,13 @@
+package vn.iostar.cinemazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaZoneApplication.class, args);
+    }
+
+}
